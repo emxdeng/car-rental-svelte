98f43e675deaf0f0d46096d7e116799d58f402d6
Emily Deng <50304670+emxdeng@users.noreply.github.com>
2024-02-27 12:30:55 +1100

    Add files via upload

---
+++ car-rental/src/routes/confirmation/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Confirmation</title>
+	<meta name="description" content="Confirmation" />
+</svelte:head>
+
+<body>
+	<br /><br />
+	<center> Your booking has been processed. </center>
+</body>
